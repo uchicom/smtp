2f33907721d4b4beb41629206e632ec234994780
Shigeki Uchiyama <shigeki.uchiyama@uchicom.com>
2016-07-28 13:57:09 +0900

    コミット漏れ

---
+++ src/main/com/uchicom/dirsmtp/Server.java
@@ -0,0 +1,18 @@
+/**
+ * (c) 2016 uchicom
+ */
+package com.uchicom.dirsmtp;
+
+/**
+ * サーバインターフェース.
+ *
+ * @author uchicom: Shigeki Uchiyama
+ *
+ */
+public interface Server {
+
+	/**
+	 * サーバの処理.
+	 */
+	public void execute();
+}
