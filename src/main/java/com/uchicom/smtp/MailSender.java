8e84ee94b6cd8a91889cd49a85feeee4646d0612
Shigeki Uchiyama <shigeki@wk2>
2017-01-10 04:49:07 +0900

    ロガーとエラー処理変更

--- src/main/java/com/uchicom/smtp/MailSender.java
+++ src/main/java/com/uchicom/smtp/MailSender.java
@@ -2,13 +2,10 @@
 package com.uchicom.smtp;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.logging.Logger;
-
-import javax.naming.NamingException;
 
 /**
  * @author uchicom: Shigeki Uchiyama
@@ -23,16 +20,20 @@ public class MailSender {
 		this.host = host;
 	}
 
-	public boolean send(String from, String to, String data) throws NamingException {
+	public void send(String from, String to, String data, String enc) throws Exception {
 
 		logger.info("transfer:" + to);
 		String[] addresses = to.split("@");
 		String[] hosts = SmtpProcess.lookupMailHosts(addresses[1]);
 		boolean sent = false;
+		Exception exception = null;
+
 		for (String host : hosts) {
+			if (sent) break;
+			exception = null;
 			try (Socket socket = new Socket(host, 25);
 					BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-					BufferedWriter writer2 = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));) {
+					OutputStreamWriter writer2 = new OutputStreamWriter(socket.getOutputStream(), enc);) {
 				logger.info("t[" + reader.readLine() + "]");
 				writer2.write("EHLO " + this.host + "\r\n");//EHLO
 				writer2.flush();
@@ -44,28 +45,43 @@ public class MailSender {
 				logger.info("mailFrom:" + from);
 				writer2.write("MAIL FROM: <" + from + ">\r\n");// MAIL FROM:
 				writer2.flush();
-				logger.info("t[" + reader.readLine() + "]");
+				rec = reader.readLine();
+				logger.info("t[" + rec + "]");
+				if (!rec.startsWith("250")) throw new Exception(from + "が迷惑メールに登録されている可能性があります。メールの設定を確認してください");
+
 				writer2.write("RCPT TO: <" + to + ">\r\n");// RCPT TO:
 				writer2.flush();
-				logger.info("t[" + reader.readLine() + "]");
+				rec = reader.readLine();
+				logger.info("t[" + rec + "]");
+				if (!rec.startsWith("250")) throw new Exception(from + "のアドレスが存在しない可能性があります。メールの設定を確認してください");
 				writer2.write("DATA\r\n");// DATA
 				writer2.flush();
-				logger.info("t[" + reader.readLine() + "]");
+				rec = reader.readLine();
+				logger.info("t[" + rec + "]");
+				if (!rec.startsWith("354")) throw new Exception("メールの送信に失敗しました。メールの管理者までご連絡ください");
 				writer2.write(data);
 				writer2.flush();
-				writer2.write(".\r\n");
+				writer2.write(".\r\n");// DATA
 				writer2.flush();
-				logger.info("t[" + reader.readLine() + "]");
+				rec = reader.readLine();
+				logger.info("t[" + rec + "]");
+				if (!rec.startsWith("250")) throw new Exception("メールの送信に失敗しました。申し訳ございませんが、info@uchicom.comまでご連絡ください");
+				sent = true;
 				writer2.write("QUIT\r\n");
 				writer2.flush();
-				logger.info("t[" + reader.readLine() + "]");
-				sent = true;
+				rec = reader.readLine();
+				logger.info("t[" + rec + "]");
 				break;
 			} catch (Exception e) {
+				e.printStackTrace();
 				logger.warning(e.getMessage());
+				exception = e;
 			}
 			logger.info("mx!:" + host);
 		}
-		return sent;
+		//送信していない場合はexceptionを送信
+		if (!sent) {
+			throw exception;
+		}
 	}
 }
